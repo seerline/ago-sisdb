37b3e6707eff710b0e042c963ac5b7d367db95b7
coollyer <zhengdong.ding@sci-inv.com>
2022-09-05 14:17:28 +0800

    corr

--- src/core/ai/sis_ai_corr.c
+++ src/core/ai/sis_ai_corr.c
@@ -35,8 +35,42 @@ double sis_ai_corrcoef(double *i1, double *i2, int size)
     }
     return SIS_IS_ZERO(r2 * r3) ? 0.0 : (r1 / sqrt(r2 * r3));
 }
+// 偏移值不大于总数据量的4/10 避免过度拟合
+double sis_ai_corrcoef_ex(double *i1, double *i2, int size, int *offset)
+{
+    int maxoff = *offset;
+    maxoff = sis_min(maxoff, size * 0.382);
+    double maxcorr = sis_ai_corrcoef(i1, i2, size);
+    *offset = 0;
+    for (int i = 1; i <= maxoff; i++)
+    {
+        double corr = sis_ai_corrcoef(i1, &i2[i], size - i);
+        if (corr >= maxcorr)
+        {
+            *offset = i;
+            maxcorr = corr;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return maxcorr;
+}
 
-#if 1
+int sis_ai_corrcoef_max(double *i1, double *i2, int size, s_sis_ai_corrcoef *corr)
+{
+    if (corr && size > 5)
+    {
+        corr->offset = size * 0.382;
+        corr->offset = sis_min(corr->offset, 1);
+        corr->corrcoef = sis_ai_corrcoef_ex(i1, i2, size, &corr->offset);
+        return 1;
+    }
+    return 0;
+}
+
+#if 0
 int main()
 {
     double in1[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};   
@@ -47,6 +81,9 @@ int main()
     printf(" %.4f \n", sis_ai_corrcoef(in1, &in2[2], 10));
     printf(" %.4f \n", sis_ai_corrcoef(in1, &in2[3], 9));
     printf(" %.4f \n", sis_ai_corrcoef(in1, &in2[4], 8));
+
+    int offset = 5;
+    printf(" %.4f offset = %d\n", sis_ai_corrcoef_ex(in1, in2, 12, &offset), offset);
     for (int i = 0; i < 12; i++)
     {
         in2[i] = in1[i] * 1000;
--- src/core/ai/sis_ai_corr.h
+++ src/core/ai/sis_ai_corr.h
@@ -1,11 +1,21 @@
 ﻿#ifndef _SIS_AI_CORR_H
 #define _SIS_AI_CORR_H
 
+typedef struct s_sis_ai_corrcoef
+{
+    int    offset;    // 第二队列相对第一队列的最优偏移值
+	double corrcoef;  // 相关系数
+} s_sis_ai_corrcoef;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 double sis_ai_corrcoef(double *i1, double *i2, int size);
+
+double sis_ai_corrcoef_ex(double *i1, double *i2, int size, int *offset);
+
+int sis_ai_corrcoef_max(double *i1, double *i2, int size, s_sis_ai_corrcoef *);
 
 #ifdef __cplusplus
 }
